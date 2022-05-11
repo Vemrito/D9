9a6802984031e4d98f1a9912c478a2b4e935e67b
Alexey Avvakumov <avv_alex@mail.ru>
2022-05-11 19:10:24 +0300

    D9

---
+++ news/migrations/0002_remove_post_link.py
@@ -0,0 +1,17 @@
+
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='post',
+            name='link',
+        ),
+    ]
